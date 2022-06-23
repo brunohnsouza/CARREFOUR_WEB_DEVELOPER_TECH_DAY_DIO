8d46b1956bdda8f5ea58083b98ee8a8e1c455bca
Bruno Henrique <brunohenriquehns@gmail.com>
2022-06-23 16:36:45 -0300

    CEP.js

--- js/CEP.js
+++ js/CEP.js
@@ -6,13 +6,13 @@
 /* Botão do CEP. Está funcional! */
 const btn_cep = document.getElementById("button__cep");
 
-/* 'cepValue' confirmará se todos os caracteres são números e 
+/* 'cepValueIsNumber' confirmará se todos os caracteres são números e 
 se o campo 'cep' foi preenchido */
 const cepValueIsNumber = (cep) => /^[0-9]+$/.test(cep); 
 
 /* 'searchCep' buscará as informações com base no CEP digitado */
-function searchCep() {
-
+function searchCep(event) {
+  if (event.type === 'click') event.preventDefault();
   const cep = document.getElementById('cep').value.replace("-","");
   const url = `https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cep}`;
   
@@ -27,7 +27,8 @@ function searchCep() {
 
 /* Ao clicar no botão do CEP (ou der um 'Enter'), 
 entrará na função 'searchCep' */
-btn_cep.addEventListener("click", () => {
-  searchCep()
-})
+btn_cep.addEventListener("click", searchCep);
+
+/* Evento adicional caso o 'click' não funcione */
+document.getElementById('cep').addEventListener('blur', searchCep);
     
